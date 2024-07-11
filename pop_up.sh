c6263f571884f292ce082ec59d4b0d632a0c853a
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-11 14:24:39 +0200

    added script

---
+++ pop_up.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+MESSAGES=(
+	"There is no spoon. - Neo"
+	"It's dangerous to go alone! Take this. - Old Man"
+	"I find your lack of faith disturbing. - Darth Vader"
+	"With great power comes great responsibility. - Uncle Ben"
+	"Do or do not. There is no try. - Yoda"
+	"Inconceivable! - Vizzini"
+	"I'm sorry, Dave. I'm afraid I can't do that. - HAL 9000"
+	"Live long and prosper. - Spock"
+	"The cake is a lie. - GLaDOS"
+	"Wibbly wobbly timey wimey. - The Doctor"
+	"Resistance is futile. - The Borg"
+	"I've seen things you people wouldn't believe. - Roy Batty"
+	"Never tell me the odds! - Han Solo"
+	"I am Groot. - Groot"
+	"Kneel before Zod. - General Zod"
+	"My precious. - Gollum"
+	"Elementary, my dear Watson. - Sherlock Holmes"
+	"To infinity and beyond! - Buzz Lightyear"
+	"Why so serious? - The Joker"
+	"May the Force be with you. - Various characters"
+	"I'm the Doctor. - The Doctor"
+	"Winter is coming. - Ned Stark"
+	"Are you suggesting coconuts migrate? - King Arthur"
+	"Nanu Nanu. - Mork"
+	"Fear is the mind-killer. - Paul Atreides"
+	"A wizard is never late, nor is he early. He arrives precisely when he means to. - Gandalf"
+	"It's over 9000! - Vegeta"
+	"I am the one who knocks! - Walter White"
+	"Hasta la vista, baby. - The Terminator"
+	"I solemnly swear that I am up to no good. - Fred and George Weasley"
+	"Get rid of pop-ups: https://codekarma42.github.io/web/"
+	"pop-ups problems? go to https://codekarma42.github.io/web/"
+	"pop-ups are a pain, go to https://codekarma42.github.io/web/"
+	"pop-ups are annoying, go to https://codekarma42.github.io/web/"
+)
+
+function show_random_message {
+    RANDOM_INDEX=$(($RANDOM % ${#MESSAGES[@]}))
+    MESSAGE=${MESSAGES[$RANDOM_INDEX]}
+    zenity --warning --title="Geek Wisdom" --text="$MESSAGE" &
+}
+
+while true; do
+	DELAY=$(shuf -i 1-60 -n 1)
+    sleep $DELAY
+    show_random_message
+
+done
