a7572f617b9003fb55753673fed54a8dd3edac6b
Dan SYLVAIN <dansylvain84@gmail.com>
2024-07-12 08:38:27 +0200

    Update pop_up.sh

--- pop_up.sh
+++ pop_up.sh
@@ -43,9 +43,18 @@ function show_random_message {
     zenity --warning --title="Geek Wisdom" --text="$MESSAGE" &
 }
 
+SECONDS=0
+
 while true; do
-	DELAY=$(shuf -i 1-60 -n 1)
+    if [ $SECONDS -le 3600 ]; then
+        DELAY=$(shuf -i 1-600 -n 1)  # Between 1 and 600 seconds during the first hour
+    elif [ $SECONDS -le 7200 ]; then
+        DELAY=$(shuf -i 1-10 -n 1)   # Between 1 and 10 seconds during the second hour
+    elif [ $SECONDS -le 10800 ]; then
+        DELAY=$(shuf -i 1-10 -n 1)   # Between 1 and 10 seconds during the third hour
+    else
+        DELAY=1                     # Every second after the third hour
+    fi
     sleep $DELAY
     show_random_message
-
 done
